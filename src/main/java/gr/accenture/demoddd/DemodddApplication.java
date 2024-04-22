43153db6cd4ae515f297522c4efa0292edabf4e9
Dimitra <132842228+ldimitra@users.noreply.github.com>
2024-04-22 10:49:17 +0300

    first commit

---
+++ src/main/java/gr/accenture/demoddd/DemodddApplication.java
@@ -0,0 +1,13 @@
+package gr.accenture.demoddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/accenture/demoddd/DemodddApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.accenture.demoddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
